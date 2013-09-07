fa8fe3f758d51f09056196e73beae7cbd97dc258
Mika <mika@LaMikaUbunteando.(none)>
2013-09-07 13:11:07 -0300

    Creación de proyecto para prototipo de conversión de archivo.

---
+++ converter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Converter::Application.config.secret_token = 'dfc5609b093bcee259bf9b846a4140d4eaff3aa9ca31170a8d7a343bd197b0d168cd10cc4dfaa6b3c4136ae6db33df7bed2ede7af2f073c23fb2c987d872d862'
