3dae18634c9d336052c154eed8f9510b61317b25
Mika <mika@LaMikaUbunteando.(none)>
2013-09-07 14:07:35 -0300

    Creación de botón para cuando se apriete se convierta un archivo.

---
+++ converter/app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+
+	def new
+	  
+	end
+  
+  def create
+    
+  end
+
+end
--- converter/config/routes.rb
+++ converter/config/routes.rb
@@ -1,4 +1,8 @@
 Converter::Application.routes.draw do
+  
+  resources :posts
+  get "welcome/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ Converter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
