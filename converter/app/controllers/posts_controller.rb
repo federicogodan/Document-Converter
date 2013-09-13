060286934fce1da5376a52f0ac5854f1703a93c2
Mikaela Pisani <mikaelapisani@gmail.com>
2013-09-13 20:10:16 -0300

    Recibe formato y se lo envia al servidor.

--- converter/app/controllers/posts_controller.rb
+++ converter/app/controllers/posts_controller.rb
@@ -5,31 +5,23 @@ class PostsController < ApplicationController
 	end
   
   def create
-      #obtener archivo y combo box 
-      
-      #esto nose donde va ! (supuestamente esta medio mal que vaya aca)
+      #get the format to be convert     
+      format = params[:post][:format]
+      #get the file 
+      #file = params[:post][:file]
       require 'socket'
       # establish connection
       ##Telling the client where to connect
       clientSession = TCPSocket.new( "localhost", 8100 )
       puts "log: starting connection"
-      #send a request
-      ## Note that this has a carriage return. Remember our server
-      ## uses the method gets() to get input back from the server. 
-      clientSession.puts "Untitled.odt pdf\n"
+      #send the request to the server
+      clientSession.puts format
       #wait for messages from the server
-      ## You've sent your message, now we need to make sure
-      ## the session isn't closed, spit out any messages the server
-      ## has to say, and check to see if any of those messages
-      ## contain 'Goodbye'. If they do we can close the connection
        while !(clientSession.closed?) &&
                 (serverMessage = clientSession.gets)
-        ## lets output our server messages
-        puts serverMessage
-        #if one of the messages contains 'Goodbye' we'll disconnect
+        #if one of the messages contains 'Completed' we'll disconnect
         ## we disconnect by 'closing' the session.
-        if serverMessage.include?("Goodbye")
-            puts "log: closing connection"
+        if serverMessage.include?("Completed")
          clientSession.close
         end
        end #end loop
--- converter/server.rb
+++ converter/server.rb
@@ -7,32 +7,24 @@ server = TCPServer.new(8100)
 # setup to listen and accept connections
 while (session = server.accept)
  #start new thread conversation
- ## Here we will establish a new thread for a connection client
+ ## Here we establish a new thread for a connection client
  Thread.start do
-   ## I want to be sure to output something on the server side
-   ## to show that there has been a connection
    puts "log: Connection from #{session.peeraddr[2]} at
           #{session.peeraddr[3]}"
    puts "log: got input from client"
-   ## lets see what the client has to say by grabbing the input
-   ## then display it. Please note that the session.gets will look
-   ## for an end of line character "\n" before moving forward.
-   input = session.gets
-   puts input
-   arr_input= input.split 
-   format = arr_input[1]
-   if (format=="txt") 
-	format = "txt:Text"
+   format = session.gets
+   format = format.rstrip
+   format = format.lstrip
+   if (format=="txt") #txt is a particular case that needs :Text after
+	   format = "txt:Text"
    end
-   to_send = '/usr/bin/libreoffice --headless --invisible --convert-to ' + format + ' ' + arr_input[0]
-   puts "to send " + to_send
+   #create the string to send to libreoffice 
+    to_send = '/usr/bin/libreoffice --headless --invisible --convert-to ' + format + ' ' + 'Untitled.odt'
+   puts to_send
    system(to_send)
-   ## Lets respond with a nice warm welcome message
    session.puts "Server: Welcome #{session.peeraddr[2]}\n"
-   # reply with goodbye
-   ## now lets end the session since all we wanted to do is
-   ## acknowledge the client
-   puts "log: sending goodbye"
-   session.puts "Server: Goodbye\n"
+   # reply   
+   puts "log: sending Completed"
+   session.puts "Completed"
  end  #end thread conversation
 end   #end loop
