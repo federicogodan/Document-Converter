aae12ac4bb97d8a20ffeceafab4010ef7400feb8
Nicolas <nicolas@MaquinaU.(none)>
2013-09-27 21:10:07 -0300

    Prueba nico

--- proyectoDevise/app/models/user.rb
+++ proyectoDevise/app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  
-  #validates_uniqueness_of :email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
