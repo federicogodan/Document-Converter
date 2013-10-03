1d308320d6b57ba587f93db7c4f85aa00f5931d1
Danilo Garcia <dani24gl@gmail.com>
2013-10-03 06:40:18 -0700

    actualizando

--- proyectoDevise/app/controllers/user/registration_controller.rb
+++ proyectoDevise/app/controllers/user/registration_controller.rb
@@ -8,9 +8,10 @@ class User::RegistrationController < Devise::RegistrationsController
     if resource.save
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_up(resource_name, resource)
-        #respond_with resource, :location => after_sign_up_path_for('user/new_file')
-        render 'user/new_file'
+        sign_up(resource_name, resource)       
+        
+        #respond_with resource, :location => after_sign_up_path_for(resource)
+        render 'user/new_file', location: "en"
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
         expire_session_data_after_sign_in!
