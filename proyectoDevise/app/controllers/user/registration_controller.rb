6ff25fcf4afcbdb2bb07b2bcae536f2821a4cad0
Nicolas <nicolas@MaquinaU.(none)>
2013-10-04 19:46:51 -0300

    Capturamos errores de email y nick ya usados

--- proyectoDevise/app/controllers/user/registration_controller.rb
+++ proyectoDevise/app/controllers/user/registration_controller.rb
@@ -4,21 +4,25 @@ class User::RegistrationController < Devise::RegistrationsController
   end
 
   def create
+    #set up profile_tyme manually
+    puts sign_up_params[:profile_type] = "user"
     build_resource(sign_up_params)
     if resource.save
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_up(resource_name, resource)       
-        
+          
         #respond_with resource, :location => after_sign_up_path_for(resource)
         render 'user/new_file'
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        
         expire_session_data_after_sign_in!
         #respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-         render 'user/home'
+        render 'user/home'
       end
     else
+      #catch error: email or nick already exists
+      set_flash_message :notice, :log_error if is_navigational_format?
       clean_up_passwords resource
       #respond_with resource
       render 'user/home'
--- proyectoDevise/app/models/user.rb
+++ proyectoDevise/app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_uniqueness_of :nick
+  
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, 
                   :remember_me, :name, :nick, :surname, :birth_date, :profile_type
