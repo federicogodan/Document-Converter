730760a18d147a8c418bf94f40cfa97cdf1c3ca4
Proyecto Ingenieria <pis2013@ubuntu.(none)>
2013-10-09 15:15:31 -0700

    cambios xa configurar capybara

--- proyectoDevise/spec/requests/pruebas_capybaras_spec.rb
+++ proyectoDevise/spec/requests/pruebas_capybaras_spec.rb
@@ -2,16 +2,27 @@ require 'spec_helper'
 
 describe "PruebasCapybaras" do
   include Capybara::DSL
+  
   describe "Sign In exito" do
     it "Sign In exitoso", :js => true do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      #visit 'localhost:3000'
       visit '/'
-      #find('Create an account').click
-      click_on 'Create an account'
-      #page.has_content? 'Create an account'
-      #get pruebas_capybaras_path
-      #response.status.should be(200)
+      #click_on 'Create an account'
+  #    click_link 'Create an account'
+      
+      
+      fill_in 'Enter email or nick', with: 'chechab'
+  #   find('#panel-register').trigger('focus')
+    
+     fill_in 'Password', with: 'holamundo'
+     click_button 'Sign in'
+  #    click_on 'Sign in'
+   #   page.execute_script("$('#panel-register').focus()")
+      #within ('form#new_user') {expect(page).to have_content('Surname')}
+      expect(page).to have_content('Signed in successfully.')
+ #     page.should have_content('Signed in successfully.')
+      #find("//input[@id='surname']").set('barreiro')
+      #find('#surname').set('barreiro')
     end
   end
 end
