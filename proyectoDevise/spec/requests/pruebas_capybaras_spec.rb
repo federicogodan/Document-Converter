98cd16216a54df87ba456109885a5e0894c82c23
chechab <cebarr.87@gmail.com>
2013-10-11 10:10:23 -0700

    Probando cosas con Capybara

--- proyectoDevise/spec/requests/pruebas_capybaras_spec.rb
+++ proyectoDevise/spec/requests/pruebas_capybaras_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "PruebasCapybaras" do
   include Capybara::DSL
+#  Capybara.using_wait_time(10) {click_link 'Register'}
   
   describe "Sign In exito" do
     it "Sign In exitoso", :js => true do
@@ -10,18 +11,37 @@ describe "PruebasCapybaras" do
       #click_on 'Create an account'
   #    click_link 'Create an account'
       
-      
-      fill_in 'Enter email or nick', with: 'chechab'
+      click_link 'Register'
+      within ('#registerModal') do
+        
+          fill_in'Email', with: 'hola@mundo.com'
+        
+      end
+ #find('#new_user').trigger('focus')
+ 
+      #find('email').set 'hola@mundo.com'
+  #    fill_in 'Email', :with => 'hola@mundo.com'
+  #    fill_in 'Nick name', :with => 'hola'
+  #    fill_in 'First name', :with => 'holita'
+  #    fill_in 'Last name', :with => 'mundito'
+  #    fill_in 'Birth Date', :with => '23-11-87'
+  #    fill_in 'Password', :with => 'pepepepe'
+  #    fill_in 'Repeat Password', :with => 'pepepepe'
+  #    click_button 'Register'
+   #   expect(page).to have_content('Welcome')
+   
+   
+#      fill_in 'Enter email or nick', with: 'chechab'
   #   find('#panel-register').trigger('focus')
     
-     fill_in 'Password', with: 'holamundo'
-     click_button 'Sign in'
+ #    fill_in 'Password', with: 'holamundo'
+ #    click_button 'Sign in'
   #    click_on 'Sign in'
    #   page.execute_script("$('#panel-register').focus()")
       #within ('form#new_user') {expect(page).to have_content('Surname')}
-      expect(page).to have_content('Signed in successfully.')
+  #    expect(page).to have_content('Signed in successfully.')
  #     page.should have_content('Signed in successfully.')
-      #find("//input[@id='surname']").set('barreiro')
+      
       #find('#surname').set('barreiro')
     end
   end
