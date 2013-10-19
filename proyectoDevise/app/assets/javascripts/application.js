9d0e038b37353c236be81164ffe6eba1374970ff
Danilo Garcia <dani24gl@gmail.com>
2013-10-19 14:47:33 -0700

    Anda sign_out. Modificado assets/javascripts/application.js,       agregando: //= require jquery //= require jquery_ujs y quitando //= require jquery_ujs_3.2.1.min

--- proyectoDevise/app/assets/javascripts/application.js
+++ proyectoDevise/app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery-2.0.3.min
+//= require jquery
+//= require jquery_ujs
+
 //= require bootstrap.min
 //= require bootstrap-datepicker
 //= require jquery.validate.min
