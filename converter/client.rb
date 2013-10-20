94887964a933387936ab055b2b8ae653c0f98473
mikaela <mikaelapisani@gmail.com>
2013-10-20 14:53:25 -0200

    Arreglos sobre client

--- converter/client.rb
+++ converter/client.rb
@@ -3,17 +3,26 @@ format = ARGV[1]
 file_name = ARGV[2]
 size=ARGV[3].to_i
 
+#TODO: get parameters through a configuration file 
+
 require 'socket'
 redirect_socket = TCPSocket.new( "localhost", 8102 )
 puts "getting server socket"
-serverMessage = redirect_socket.gets
-puts "serverMessage : " + serverMessage
-clientSession = TCPSocket.new( "localhost", serverMessage.to_i)
+server_ip = redirect_socket.gets.delete("\n")
+puts "ip: "
+puts server_ip 
+server_port = redirect_socket.gets.delete("\n").to_i
+puts "port: " 
+puts server_port
+puts "connecting to server"
+puts server_ip 
+puts server_port
+clientSession = TCPSocket.new( server_ip, server_port)
 puts "sending ACK"
 redirect_socket.puts "ACK" 
 clientSession.puts format
 clientSession.puts file_name
-system "chmod 777 " + file_path
+#system "sudo chmod 777 " + file_path
 
 clientSession.puts size
 File.open(file_path, 'r') do |file|  
@@ -23,6 +32,7 @@ File.open(file_path, 'r') do |file|
    end
    clientSession.write(file.read(size))      
 end
+puts "waiting response"
 serverMessage = clientSession.gets
 puts "Recieved: " 
 puts serverMessage
