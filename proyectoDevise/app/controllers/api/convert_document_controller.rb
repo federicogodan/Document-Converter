a6a5dca118a5c25528b831f409f11c1c53f64e73
Danilo Garcia <dani24gl@gmail.com>
2013-11-14 16:43:46 -0800

    document controller

--- proyectoDevise/app/controllers/api/convert_document_controller.rb
+++ proyectoDevise/app/controllers/api/convert_document_controller.rb
@@ -1,17 +1,21 @@
 class Api::ConvertDocumentController < ApplicationController#ApiController
-  
   def create
+    render :nothing=>true, :status => 200, :content_type => 'text/html'
+  end
+  
+  def test
     
-    @current_user = User.first
-    puts params.to_json
-    
+    puts "!"*50
+    @current_user = User.find_by_nick('userexample1')
+    puts params[:document].to_json
+    puts "/"*50
     #Previous checks to prevent null values 
-    if params[:file]
-      file_name = params[:file].original_filename
-      file_content = params[:file] 
+    if params[:document][:file]
+      file_name = params[:document][:file].original_filename
+      file_content = params[:document][:file] 
       f_size = file_content.size if file_content
     end    
-    destiny_format_name = params[:destination_format]    
+    destiny_format_name = params[:document][:destination_format]    
     destiny_format = Format.find_by_name(destiny_format_name) if destiny_format_name # != ''
     has_extension = File.extname(file_name).split('.')[1] if file_name
     ext = has_extension.upcase if has_extension   
@@ -21,28 +25,30 @@ class Api::ConvertDocumentController < ApplicationController#ApiController
     if (file_name && file_content && f_size && destiny_format && @current_user && ext)
       #Creating association between the user and the document uploaded. Also creating the converted document's object
   
-      document = Document.new(name:file_name,expired:false,size:f_size)
-      document.file = params[:file]  
-      puts document
+      @document = Document.new(name:file_name,expired:false,size:f_size)
+      @document.file = params[:document][:file]  
+      puts @document
       
       origin_format = Format.find_by_name(destiny_format_name) if destiny_format_name
   
-      document.format = origin_format
-      document.user = @current_user
-      document.converted_document = ConvertedDocument.new
-      document.converted_document.set_to_converting
-      document.converted_document.format = destiny_format
+      @document.format = origin_format
+      @document.user = @current_user
+      @document.converted_document = ConvertedDocument.new
+      @document.converted_document.set_to_converting
+      @document.converted_document.format = destiny_format
     else
       valid_parameters = false
     end  
 
     respond_to do |f|
-      if valid_parameters && document.save && document.converted_document.save
+      if valid_parameters #&& @document.save && @document.converted_document.save
         puts "Document created"
-        f.json { render json: document, status: :created }
+        puts "rerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"
+        f.json { render json: @document, status: :created }
       else
         puts "Error in the validation of the document's parameters"
-        f.json { render json: document.errors, status: :unprocessable_entity }
+        f.html {redirect_to '/user/dashboard'}
+        f.json { render json: @document.errors, status: :unprocessable_entity }
       end
     end
   end
