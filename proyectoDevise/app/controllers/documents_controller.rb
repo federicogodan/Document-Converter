2cd1ab197a91cae09596fb37a382d1717952fb00
mikaela <mikaelapisani@gmail.com>
2013-11-17 03:39:04 -0200

    Cambios para bajar de url

--- proyectoDevise/app/controllers/documents_controller.rb
+++ proyectoDevise/app/controllers/documents_controller.rb
@@ -54,13 +54,25 @@ class DocumentsController < ApplicationController
     
     #RestClient.post 'http://localhost:3000/api/convert_document/', :document => { :file => params[:document][:file],   :destination_format => params[:document][:destination_format] }
     #RestClient.post 'http://localhost:3000/api/convert_document/', :document => {  :destination_format => params[:document][:destination_format] }
-   
+     if params[:document][:upload_method] == 'URL'
+       @file_name =  File.basename(URI.parse(params[:document][:url]).path)
+       File.open(@file_name, 'wb') do |fo|
+          fo.write(open(params[:document][:url]).read)
+       end
+       @file_content = File.open("./" + @file_name)
+       #@f_size = @file_content.size if @file_content      
+    else 
+      @file_name = params[:document][:file].original_filename
+      @file_content = params[:document][:file] 
+      #@f_size = @file_content.size if @file_content
+    end 
+    
     request = RestClient::Request.new(
           :method => :post,
           :url => 'http://localhost:3000/api/convert_document/',
           :payload => {
             :multipart => true,
-            :document => { :file => params[:document][:file],
+            :document => { :file =>  @file_content,
               :destination_format => params[:document][:destination_format]
               }
           })      
