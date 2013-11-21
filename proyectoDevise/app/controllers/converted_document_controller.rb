9af65b8655310d75718c011eac24339dd0f4ce99
Nicolas <nicolas@MaquinaU.(none)>
2013-11-21 21:46:12 -0200

    bajando la ultima version

--- proyectoDevise/app/controllers/converted_document_controller.rb
+++ proyectoDevise/app/controllers/converted_document_controller.rb
@@ -4,7 +4,7 @@ class ConvertedDocumentController < ApplicationController
   
   #remove expired documents from S3
   def self.delete_old_files
-      cds =  ConvertedDocument.joins('INNER JOIN documents ON (converted_documents.document_id = documents.id) INNER JOIN users ON (documents.user_id = users.id) where converted_documents.created_at <= now() - users.documents_time_for_expiration && converted_document.status =2')    
+      cds =  ConvertedDocument.joins('INNER JOIN documents ON (converted_documents.document_id = documents.id) INNER JOIN users ON (documents.user_id = users.id) where converted_documents.created_at <= now() - users.documents_time_for_expiration && converted_documents.status =2')    
     puts cds.count
     
     cds.each do |document|
