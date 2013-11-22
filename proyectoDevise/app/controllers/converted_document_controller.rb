55f7c95263f2e5fc8d423c21f06fce20d0ef819e
Nicolas <nicolas@MaquinaU.(none)>
2013-11-22 06:48:16 -0200

    arreglamdo el borrado periodico

--- proyectoDevise/app/controllers/converted_document_controller.rb
+++ proyectoDevise/app/controllers/converted_document_controller.rb
@@ -4,7 +4,7 @@ class ConvertedDocumentController < ApplicationController
   
   #remove expired documents from S3
   def self.delete_old_files
-      cds =  ConvertedDocument.joins('INNER JOIN documents ON (converted_documents.document_id = documents.id) INNER JOIN users ON (documents.user_id = users.id) where converted_documents.created_at <= now() - users.documents_time_for_expiration')    
+      cds =  ConvertedDocument.joins('INNER JOIN documents ON (converted_documents.document_id = documents.id) INNER JOIN users ON (documents.user_id = users.id) where converted_documents.created_at <= now() - users.documents_time_for_expiration;')    
     
     cds.each do |conv_doc|
       AWS::S3::Base.establish_connection!(:access_key_id => 'AKIAI5XVTWY2S636HMWQ', :secret_access_key => '/B1J/ZLzMgqcyLAbvLlgVyPdzwh6WlDRZd7rEYyG')
@@ -16,7 +16,7 @@ class ConvertedDocumentController < ApplicationController
       #AWS::S3::DEFAULT_HOST.replace 'magicrepository.s3-website-sa-east-1.amazonaws.com'
       
       doc = Document.find(conv_doc.document_id)
-      
+      cd = ConvertedDocument.find(conv_doc.id)
       if conv_doc.status == 2
         
         length = doc.name.split('.').length
@@ -26,11 +26,15 @@ class ConvertedDocumentController < ApplicationController
         
         doc_name += '.' + format_name
         
+        cd.set_to_expired
+        cd.save
+        
         AWS::S3::S3Object.delete("uploads/document/file/#{doc.id}/#{doc_name}","magicrepository")
-        conv_doc.set_to_expired 
-      elsif document.status == 0
+        
+      elsif conv_doc.status == 0
         AWS::S3::S3Object.delete("uploads/document/file/#{doc.id}/#{doc.name}","magicrepository")
-        conv_doc.set_to_failed
+        cd.set_to_failed
+        cd.save
       end
     end
   end
